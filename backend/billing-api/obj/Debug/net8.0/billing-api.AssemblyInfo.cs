b5ab0bf9130b02c94677c3359ac3010029f5b9ce
TAMILSELVANPOOKZE <tamilselvan28032000@gmail.com>
2026-03-12 19:17:53 +0530

    Fix container port from 80 to 8080

--- backend/billing-api/obj/Debug/net8.0/billing-api.AssemblyInfo.cs
+++ backend/billing-api/obj/Debug/net8.0/billing-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("billing-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2626475c1a383c4351b127a64dc06bbdd3db7e59")]
 [assembly: System.Reflection.AssemblyProductAttribute("billing-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("billing-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
