2f71ffc3c9c768195e0509bd209f1a61f34b7bcd
TAMILSELVANPOOKZE <tamilselvan28032000@gmail.com>
2026-03-13 10:39:41 +0530

    Fix frontend folder tracking

---
+++ frontend/billing-ui/src/App.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+
+  const [bills, setBills] = useState([]);
+
+  useEffect(() => {
+    fetch("http://billing-api-service/api/billing")
+      .then(res => res.json())
+      .then(data => setBills(data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Billing Application</h1>
+
+      <table border="1">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Item</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {bills.map(b => (
+            <tr key={b.id}>
+              <td>{b.id}</td>
+              <td>{b.item}</td>
+              <td>{b.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
